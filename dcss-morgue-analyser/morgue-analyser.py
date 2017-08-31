0176bfda439c03cbb2802af376d4adeb945655f0
MarshP <mampenman+github@gmail.com>
2017-08-31 15:37:37 +0100

    documentation testing

--- dcss-morgue-analyser/morgue-analyser.py
+++ dcss-morgue-analyser/morgue-analyser.py
@@ -1,33 +1,34 @@
 import sys, os, logging
 
 LOG_FORMAT = "%(levelname)s - %(message)s - %(asctime)s"
-logging.basicConfig(filename = ".\\morgue-analyser.log",
-    level= logging.DEBUG,
-    format = LOG_FORMAT,
-    filemode = 'w')
+logging.basicConfig(filename=".\\morgue-analyser.log",
+                    level=logging.DEBUG,
+                    format=LOG_FORMAT,
+                    filemode='w')
 logger = logging.getLogger()
 
-#TODO change the cwd to that given in the cl args. If that cannot be done put a
-# meaningful message to output.
-# (sys.argv[1:]))
+# TODO in future change the cwd to that given in the cl args. If that cannot be done put a meaningful message to output.
+# (sys.argv[1:])
 
-#TODO read the files in cwd ending in .txt into some sorta buffer
+argument_1 = str(sys.argv[1:])
+print("Argument 1: ",argument_1)
+print("Argument 1 is a ",type(argument_1))
+os.chdir(argument_1)
+# print ("Current working dir: %s" % os.getcwd())
 
-#TODO parse the files and report
+# TODO read the files in cwd ending in .txt into some sorta buffer
 
-#TODO write to an output file in the morgue - write logs and program output.
+# TODO parse the files and report
+
+# TODO write to an output file in the morgue - write logs and program output.
 
 output_variable = "This string is in a variable to be replaced by meaningful program output later."
 results_file = open(".\\morgue-analysis.txt", 'w')
 logger.info("Opened output for writing")
 results_file.write(output_variable)
-#TODO handle cannot male or write errors - permissions etc
-results_file.close
+# TODO handle cannot male or write errors - permissions etc
+results_file.close()
 logger.info("Closed output file")
-
-
-
-
 
 logger.debug("This is a test debug level log message")
 logger.info("This is a test info level log message")
