d10a53f1fa3b0faf4bc2fac0dfa0dd754f816006
MarshP <mampenman+github@gmail.com>
2017-09-01 22:53:02 +0100

    Gold stats completed - an easy quick win.

--- dcss-morgue-analyser/data_collection_functions.py
+++ dcss-morgue-analyser/data_collection_functions.py
@@ -1,13 +1,62 @@
 import re
 
-def get_gold_stats(buffer):
-    # GOLD_COUNT
-    gold_total = 0
-    GoldCountRegex = re.compile(r'(Gold:(\s+))(\d+)')
-    gold_match = GoldCountRegex.search(buffer)
-    print('Gold amount: ' + gold_match.group(3))
-    gold_total += int(gold_match.group(3))
-    print(gold_total)
+def collect_data(buffer, directory_path):
+    with open((directory_path + '\\' + 'dcssma-analysis.txt'), 'w') as myfile:
+        myfile.write("# DCSS Morgue Analyser Output #\n\n"
+                     "**Note:** 'Average' means 'arithmetic mean' throughout.\n\n")
+    get_gold_stats(buffer, directory_path)
+
+# TODO How far does the player usually progress, in terms of dungeon exploration, time/turns played, XP/XL and skills?
+# TODO What usually kills the characters, and how, and where?
+# TODO Are there avoidable issues? Including:
+    # TODO unidentified items while i.d scrolls are available,
+    # TODO unused heal wounds pots at HP death,
+    # TODO under-used god abilities with high piety,
+    # TODO unspent gold
+    # TODO enchant armour/weaps scrolls gained early and unused
+# TODO ...and other gameplay smells to be added
+# TODO What is the progress on primary skills for the background and species, or the skills with best species aptitude?
+# TODO Later convert all this to some NLP or parsing library made for the job
+
+
+
+def get_gold_stats(buffer, directory_path):
+    gold_at_death_sum = 0
+    gold_at_death_avg = 0
+    gold_spent_sum = 0
+    gold_spent_avg = 0
+    gold_spent_pc = 0
+
+    gold_at_death_regex = re.compile(r'(Gold:(\s+))(\d+)')
+    gold_at_death_matches = re.findall(gold_at_death_regex, buffer)
+    for match in gold_at_death_matches:
+        gold_at_death_sum += int(match[2])
+    gold_at_death_avg = gold_at_death_sum/len(gold_at_death_matches)
+
+    gold_spent_regex = re.compile(r'(spent(\s+))(\d+)')
+    gold_spent_matches = re.findall(gold_spent_regex, buffer)
+    for match in gold_spent_matches:
+        gold_spent_sum += int(match[2])
+    gold_spent_avg = gold_spent_sum/len(gold_spent_matches)
+
+    gold_spent_pc = (gold_spent_avg/(gold_spent_avg+gold_at_death_avg))*100
+    print("At death ",gold_at_death_sum, gold_at_death_avg)
+    print("Spent ", gold_spent_sum, gold_spent_avg)
+    print("%.0f" % gold_spent_pc+"%")
+
+    with open((directory_path + '\\' + 'dcssma-analysis.txt'), 'a') as myfile:
+        myfile.writelines(["## Gold ##\n\n",
+                           "Avg. gold at death: " + str(gold_at_death_avg)+"\n",
+                           "Avg. gold spent: " + str(gold_spent_avg) + "\n\n"
+                           "Gold only helps you win if you spend it.\nYou spent on average "+ str(round(gold_spent_pc,))+"% "+
+                           "of the gold you collected.\n\n"])
+
+                     # "Avg. gold spent: ", gold_spent_avg, "\n")
+                     # "Gold only helps you win if you spend it. You spent on average ", gold_spent_pc, "%"
+                     # "of the gold you collect. You cannott take it with you.\n")
+                     #
+
+
     # do someting meaningful later
     # ANOTHER_THING
     ##AnotherCountRegex = re.compile(r'Gold:(\s+)\d+')
@@ -19,5 +68,3 @@ def get_gold_stats(buffer):
 
 
 
-def collect_data(buffer):
-    get_gold_stats(buffer)
--- dcss-morgue-analyser/morgue-analyser.py
+++ dcss-morgue-analyser/morgue-analyser.py
@@ -1,11 +1,11 @@
-import sys, os, logging, glob, fnmatch, re
-from data_collection_functions import *
-
-# TODO Learn to use a debugger. Begin to use it in the dev process.
-
+import fnmatch
 # TODO Learn more on logging best practice. Decide how to log the application and whether to use separate modules.
 # Logging module?
 import logging
+import os
+
+from data_collection_functions import *
+
 
 def start_logging():
     LOG_FORMAT = "%(levelname)s - %(message)s - %(asctime)s"
@@ -15,56 +15,65 @@ def start_logging():
                         filemode='w')
     logger = logging.getLogger()
 
+
 start_logging()
+
 
 # TODO Learn to make and run tests, and do so once we have some functions
 
 # TODO read the files in cwd ending in .txt into some sorta concatenated buffer or even a new, big, file
 
-def read_morgue_files(directory_path):
-    buffer = ''
-    # may need a rethink if memory issues
+
+# tempfiles is a list of file handles to your temp files. Order them however you like
+
+def join_morgue_files(directory_path):
+    ConcatenatedMorgueFile = open((directory_path + '\\' + 'dcssma-temp-file.txt'), 'w')
     for filename in os.listdir(directory_path):
         if fnmatch.fnmatch(filename, '*.txt'):
-            WorkingFile = open((directory_path+'\\'+filename),'r')
-            buffer = WorkingFile.read()
-            # calls to stat collection fns go here
-            collect_data(buffer)
-            # WOuld one big fn be better? Pass the buffer once? But not as modular.
-            WorkingFile.close()
+            WorkingFile = open((directory_path + '\\' + filename), 'r')
+            ConcatenatedMorgueFile.write(WorkingFile.read())
+    ConcatenatedMorgueFile.close()
 
 
+join_morgue_files('..\\tests\\test-data')
 
-read_morgue_files('..\\tests\\test-data')
+
 # change to '.' for cwd after testing
 
+def send_data_to_process(directory_path):
+    SendingFileHandle = open((directory_path + '\\' + 'dcssma-temp-file.txt'), 'r')
+    buffer = SendingFileHandle.read()
+    collect_data(buffer, directory_path)
+    SendingFileHandle.close()
+    os.remove(directory_path + '\\' + 'dcssma-temp-file.txt')
 
+
+# TODO fix encoding - results files are ansi
+
+send_data_to_process('..\\tests\\test-data')
+# change to '.' for cwd after testing
 
 # TODO A fn for each regex so they can be modularly added
-
-
-
 
 # ContentToParse = open(os.path.relpath(
 #     file_path))
 # print(ContentToParse.readlines())
 # content_to_parse = open(os.path.relpath(,).\\.\\tests\\test-data\\morgue-Mash-20170822-155351.txt)
 
-
 # TODO parse the files against searched terms and report
 
 # TODO write to an output file in the morgue - write logs and program output.
 
-#output_variable = "This string is in a variable to be replaced by meaningful program output later."
-#results_file = open(".\\morgue-analysis.log", 'w')  # later.txt or .md
-#logger.info("Opened output for writing")
-#results_file.write(output_variable)
+# output_variable = "This string is in a variable to be replaced by meaningful program output later."
+# results_file = open(".\\morgue-analysis.log", 'w')  # later.txt or .md
+# logger.info("Opened output for writing")
+# results_file.write(output_variable)
 # TODO handle possible errors throughout. Cannot make file or write errors - permissions etc.
-#results_file.close()
-#logger.info("Closed output file")
+# results_file.close()
+# logger.info("Closed output file")
 
-#logger.debug("This is a test debug level log message y")
-#logger.info("This is a test info level log message")
-#logger.warning("This is a test warning level log message")
-#logger.error("This is a test error level log message")
-#logger.critical("This is a test critical level log message")
+# logger.debug("This is a test debug level log message y")
+# logger.info("This is a test info level log message")
+# logger.warning("This is a test warning level log message")
+# logger.error("This is a test error level log message")
+# logger.critical("This is a test critical level log message")
